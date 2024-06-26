9fe9dcf8d57089f23869ab3ce24e619993eb996b
William <williwiser@gmail.com>
2024-06-26 03:44:40 +0200

    fix title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Kutanga - Tax & Accouting",
+  title: "Kutanga - Tax & Accounting",
   description: "Financial strategies and business plan advice.",
 };
 
